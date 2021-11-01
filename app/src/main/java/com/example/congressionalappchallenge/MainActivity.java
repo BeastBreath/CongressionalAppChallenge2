e22adbdd4c9d7eaa7fc27ee9b8cdc1a08aba19f9
Nividh S <67477539+BeastBreath@users.noreply.github.com>
2021-10-31 19:59:01 -0700

    Initial commit

---
+++ app/src/main/java/com/example/congressionalappchallenge/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.congressionalappchallenge;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
